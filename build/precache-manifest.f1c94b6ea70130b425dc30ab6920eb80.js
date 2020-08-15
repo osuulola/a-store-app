07ecc943811958f5d70f203e99edaa344ec33987
osuulola <osuulola@yahoo.com>
2020-08-15 09:15:02 +0100

    comited

---
+++ build/precache-manifest.f1c94b6ea70130b425dc30ab6920eb80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a90bd95a5ed2f19cc3b23c5197d5d5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6971901ec7b92299df1",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "50ea6e2a2e6563d95d22",
+    "url": "/static/css/main.dc6ddeb4.chunk.css"
+  },
+  {
+    "revision": "a6971901ec7b92299df1",
+    "url": "/static/js/2.e41a07c7.chunk.js"
+  },
+  {
+    "revision": "50ea6e2a2e6563d95d22",
+    "url": "/static/js/main.09862fcd.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
